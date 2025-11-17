280e996f384f077e08a5e8b816c884d1152d5967
Mohamed AFKIR <afkir.mohamed2708@gmail.com>
2025-11-17 03:36:47 +0100

    feat(cart): display cart entries/items

--- frontend/src/app/features/cart/cart.page.ts
+++ frontend/src/app/features/cart/cart.page.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { FooterComponent } from '@/app/core/components/footer/footer.component';
 import { NavbarComponent } from '@/app/core/components/navbar/navbar.component';
 import { CartService } from '@/app/core/services/cart.service';
+import { Observable } from 'rxjs';
+import { Cart } from '@/app/core/models/cart.model';
 
 @Component({
   selector: 'app-cart-page',
   imports: [NavbarComponent, FooterComponent],
   templateUrl: './cart.page.html',
 })
-export class CartPage {
+export class CartPage implements OnInit {
+  cart = signal<Cart | undefined>(undefined);
 
   constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getCartAsObservable().subscribe({
+      next: (cart) => this.cart.set(cart)
+    })
+  }
 
 }
