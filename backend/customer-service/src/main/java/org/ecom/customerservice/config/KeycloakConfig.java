65fc130307e7db114e570afc4fd1942a7feaaf52
Mohamed AFKIR <afkir.mohamed2708@gmail.com>
2025-12-05 18:00:15 +0100

    log(customer): log keycloak props

--- backend/customer-service/src/main/java/org/ecom/customerservice/config/KeycloakConfig.java
+++ backend/customer-service/src/main/java/org/ecom/customerservice/config/KeycloakConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class KeycloakConfig {
@@ -16,6 +18,8 @@ public class KeycloakConfig {
 
     @Bean
     public Keycloak keycloak() {
+        log.info("Configuring Keycloak with server URL: {}, realm: {}, clientId: {}",
+                properties.url(), properties.realm(), properties.clientId());
         return KeycloakBuilder.builder()
                 .serverUrl(properties.url())
                 .realm(properties.realm())
